34febcc158e79a95373e14f506dfc66d69b68829
JoonHoSeong <joonho1366@gmail.com>
2024-07-19 02:02:42 +0900

    [Delete] Flake8 in gitacitions

--- Scraping/encryption_db_info.py
+++ Scraping/encryption_db_info.py
@@ -1,16 +1,18 @@
 from cryptography.fernet import Fernet
 
+
 def encrypt_config(config_file, key_file):
-    with open(key_file, 'rb') as f:
+    with open(key_file, "rb") as f:
         key = f.read()
     fernet = Fernet(key)
 
-    with open(config_file, 'rb') as f:
+    with open(config_file, "rb") as f:
         original_config = f.read()
     encrypted_config = fernet.encrypt(original_config)
 
-    with open(config_file + '.enc', 'wb') as f:
+    with open(config_file + ".enc", "wb") as f:
         f.write(encrypted_config)
 
-encrypt_config('config.json', 'config.key')
-encrypt_config('config_test.json', 'config.key')
+
+encrypt_config("config.json", "config.key")
+encrypt_config("config_test.json", "config.key")
--- Scraping/insert_ingredient_category.py
+++ Scraping/insert_ingredient_category.py
@@ -1,24 +1,27 @@
 import json
+
 from cryptography.fernet import Fernet
 
+
 def load_config(config_file, key_file):
-    with open(key_file, 'rb') as f:
+    with open(key_file, "rb") as f:
         key = f.read()
     fernet = Fernet(key)
 
-    with open(config_file, 'rb') as f:
+    with open(config_file, "rb") as f:
         encrypted_config = f.read()
     decrypted_config = fernet.decrypt(encrypted_config)
 
     return json.loads(decrypted_config)
 
+
 # 테스트를 위한 코드
-config = load_config('config_test.json.enc', 'config.key')
+config = load_config("config_test.json.enc", "config.key")
 # 본 코드
 # config = load_config('config.json.enc', 'config.key')
-endpoint = config['endpoint']
-username = config['username']
-password = config['password']
+endpoint = config["endpoint"]
+username = config["username"]
+password = config["password"]
 
 # RDS 연결 로직 (예시)
 import psycopg2
@@ -31,48 +34,54 @@ conn = psycopg2.connect(
 
 
 # 대분류 코드 넣기
-with open('../DataTools/Data/result/ingredients_major.json', 'r') as f :
+with open("../DataTools/Data/result/ingredients_major.json") as f:
     data = json.load(f)
 cur = conn.cursor()
-for item in data :
-    code = data[item]['major_code']
-    name = data[item]['major_name']
+for item in data:
+    code = data[item]["major_code"]
+    name = data[item]["major_name"]
     cur.execute("INSERT INTO ingre_major (name, id) VALUES (%s, %s)", (name, code))
 conn.commit()
 print("데이터 삽입 완료")
 
 
 # 중분류 코드 넣기
-with open('../DataTools/Data/result/ingredients_middle.json', 'r') as f :
+with open("../DataTools/Data/result/ingredients_middle.json") as f:
     data = json.load(f)
 cur = conn.cursor()
 temp = []
-for item in data :
-    code = data[item]['middle_code']
-    name = data[item]['middle_name']
-    major_code = data[item]['major_code']
-    if code not in temp :
-        cur.execute("INSERT INTO ingre_middle (name, id, major_id) VALUES (%s, %s, %s)", (name, code, major_code))
+for item in data:
+    code = data[item]["middle_code"]
+    name = data[item]["middle_name"]
+    major_code = data[item]["major_code"]
+    if code not in temp:
+        cur.execute(
+            "INSERT INTO ingre_middle (name, id, major_id) VALUES (%s, %s, %s)",
+            (name, code, major_code),
+        )
         print(name, code, major_code)
         temp.append(code)
-        
+
 conn.commit()
 print("데이터 삽입 완료")
 
 
 # 소분류 코드 넣기
-with open('../DataTools/Data/result/ingredients_sub.json', 'r') as f :
+with open("../DataTools/Data/result/ingredients_sub.json") as f:
     data = json.load(f)
 cur = conn.cursor()
 temp = []
-for item in data :
-    code = data[item]['sub_code']
-    name = data[item]['sub_name']
-    middle_code = data[item]['middle_code']
-    if name == None :
-        name = 'None'
-    if code not in temp :
-        cur.execute("INSERT INTO ingre_sub (name, id, middle_id) VALUES (%s, %s, %s)", (name, code, middle_code))
+for item in data:
+    code = data[item]["sub_code"]
+    name = data[item]["sub_name"]
+    middle_code = data[item]["middle_code"]
+    if name == None:
+        name = "None"
+    if code not in temp:
+        cur.execute(
+            "INSERT INTO ingre_sub (name, id, middle_id) VALUES (%s, %s, %s)",
+            (name, code, middle_code),
+        )
         print(name, code, middle_code)
         temp.append(code)
 conn.commit()
