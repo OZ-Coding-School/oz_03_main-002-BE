b0d395704c9c352eb86b4a71bcf017102cc577a4
JoonHoSeong <joonho1366@gmail.com>
2024-07-19 01:15:02 +0900

    [fix,add](scraping, data analysis) : fix data analysis code, add category data insert code

--- DataTools/get_ingredients_data.py
+++ DataTools/get_ingredients_data.py
@@ -37,29 +37,28 @@ with open("Data/result/ingredients_major.json", "w", encoding="utf-8") as f:
     f.write(result_json)
 
 # 중분류 코드
-middle_code = (
-    df_deduplicated[["식품중분류코드", "식품중분류명"]]
-    .drop_duplicates(subset="식품중분류명", keep="first")
-    .reset_index(drop=True)
-)
+middle_code = df[['식품대분류코드', '식품중분류명','식품중분류코드']]
+
 middle_code = middle_code.rename(
-    columns={"식품중분류코드": " middle_code", "식품중분류명": "middle_name"}
+    columns={"식품대분류코드" : 'major_code', "식품중분류코드": "middle_code", "식품중분류명": "middle_name"}
 )
 result_dict = middle_code.to_dict(orient="index")
+result_json = json.dumps(result_dict, ensure_ascii=False)
 result_json = json.dumps(result_dict, ensure_ascii=False)
 with open("Data/result/ingredients_middle.json", "w", encoding="utf-8") as f:
     f.write(result_json)
 
+
 # 소분류 코드
 sub_code = (
-    df_deduplicated[["식품소분류코드", "식품소분류명"]]
-    .drop_duplicates(subset="식품소분류명", keep="first")
+    df[["식품중분류코드","식품소분류코드", "식품소분류명"]]
     .reset_index(drop=True)
 )
 sub_code = sub_code.rename(
-    columns={"식품소분류코드": " sub_code", "식품소분류명": "sub_name"}
+    columns={"식품중분류코드" : "middle_code","식품소분류코드": "sub_code", "식품소분류명": "sub_name"}
 )
 result_dict = sub_code.to_dict(orient="index")
+
 result_json = json.dumps(result_dict, ensure_ascii=False)
 with open("Data/result/ingredients_sub.json", "w", encoding="utf-8") as f:
     f.write(result_json)
