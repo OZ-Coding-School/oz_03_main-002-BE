7f112862fb9e687039ae0b86c33d5d6429481e8e
JoonHoSeong <joonho1366@gmail.com>
2024-07-28 04:52:40 +0900

    [Feature](Social Login) : 소셜 로그인 Viewset화, Django admin App_users 버그 fix

--- Django/app_user/admin.py
+++ Django/app_user/admin.py
@@ -1,27 +1,36 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin, UserChangeForm
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from .models import App_User
 
 
+class CustomUserChangeForm(UserChangeForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = App_User
+        fields = "__all__"
+        exclude = ("date_joined",)  # date_joined 필드 제외
+
+
 class CustomUserAdmin(UserAdmin):
-    # 기본 UserAdmin 클래스를 상속하고 필요에 따라 커스터마이징합니다.
+    form = CustomUserChangeForm  # 수정된 폼 사용
     fieldsets = (
         (None, {"fields": ("user_id", "password")}),
-        (("Personal info"), {"fields": ("name", "nick_name", "email")}),
+        (("Personal info"), {"fields": ("username", "nick_name", "email")}),
         (
             ("Permissions"),
             {
                 "fields": (
                     "is_active",
-                    "is_staff",
                     "is_superuser",
                     "groups",
                     "user_permissions",
                 ),
             },
         ),
-        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (("Important dates"), {"fields": ("last_login",)}),  # date_joined 제거
     )
     list_display = ("id", "user_id", "username", "nick_name", "is_staff")
     search_fields = ("id", "user_id", "username", "nick_name")
--- Django/app_user/urls.py
+++ Django/app_user/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from . import views
-from .views import CompleteSocialSignupView
-from .views import GoogleLoginCallbackView
-from .views import google_login
+# from .views import CompleteSocialSignupView
+# from .views import GoogleLoginCallbackView
+from .views import GoogleLoginViewSet, CompleteSocialSignupViewSet
 
 app_name = "user"
 
@@ -23,16 +23,23 @@ router.register(
 router.register(
     "refrigerator", views.UserRefrigeratorViewSet, basename="user_refrigerator"
 )  # UserRefrigeratorViewSet 등록
+########
+router.register('google', views.GoogleLoginViewSet, basename='google_login')
+router.register('complete-signup', views.CompleteSocialSignupViewSet, basename='complete_social_signup')
+########
 
 urlpatterns = [
     path("", include(router.urls)),
     # 구글 소셜로그인
-    path("google/login/", google_login, name="google_login"),
-    path("google/callback/", GoogleLoginCallbackView.as_view(), name="google_callback"),
-    # path('google/login/finish/', CompleteSocialSignupView.as_view(), name='google_login_todjango'),
-    path(
-        "complete_social_signup/",
-        CompleteSocialSignupView.as_view(),
-        name="complete_social_signup",
-    ),  # 추가 정보 입력 폼 URL 패턴
+    # path("google/login/", google_login, name="google_login"),
+    # path("google/callback/", GoogleLoginCallbackView.as_view(), name="google_callback"),
+    # # path('google/login/finish/', CompleteSocialSignupView.as_view(), name='google_login_todjango'),
+    # path(
+    #     "complete_social_signup/",
+    #     CompleteSocialSignupView.as_view(),
+    #     name="complete_social_signup",
+    # ),  # 추가 정보 입력 폼 URL 패턴
 ]
+
+
+
