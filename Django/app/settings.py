4791e201fd33fe2dfc26aec34101ab4af9ad4047
JoonHoSeong <joonho1366@gmail.com>
2024-07-30 03:54:18 +0900

    [Fix](GitActions) : migration오류 수정

--- Django/app/settings.py
+++ Django/app/settings.py
@@ -126,6 +126,8 @@ REST_FRAMEWORK = {
     ),
 }
 
+SITE_ID=1
+
 # Google OAuth 설정 (클라이언트 ID, 시크릿 키)
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
